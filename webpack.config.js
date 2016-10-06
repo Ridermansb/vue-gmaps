bc530c156f2f2761e2c431a7e3edf9b2ed8c858b
ridermansb <ridermansb@gmail.com>
2016-10-06 13:05:14 -0300

    fix(): Try add umd library target
    Add umd library target as suggest in forum to fix error when try use plugin

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,8 @@ module.exports = {
   entry: './src/index.js',
   output: {
     path: path.join(__dirname, 'dist'),
-    filename: 'index.js'
+    filename: 'index.js',
+    libraryTarget: 'umd'
   },
   module: {
     preLoaders: [
