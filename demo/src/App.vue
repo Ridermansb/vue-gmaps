3082d085fb0d37207d6d0bdedbf9a55ed7e059f2
ridermansb <ridermansb@gmail.com>
2016-10-14 22:51:31 -0300

    docs(): Add Project demo

---
+++ demo/src/App.vue
@@ -0,0 +1,30 @@
+<template xmlns:v-gmaps-searchbox="http://www.w3.org/1999/xhtml">
+    <div id="app">
+        <img src="./assets/logo.png">
+        <h1>{{ msg }}</h1>
+        <input v-model=vm.searchPlace v-gmaps-searchbox:location=vm>
+        <pre>{{ vm | json }}</pre>
+    </div>
+</template>
+
+<script>
+    export default {
+        data () {
+            return {
+                msg: 'Hello Vue Maps!',
+                vm: {
+                    searchPlace: '',
+                    location: {}
+
+                }
+            }
+        },
+
+    }
+</script>
+
+<style>
+    body {
+        font-family: Helvetica, sans-serif;
+    }
+</style>
---
+++ demo/src/main.js
@@ -0,0 +1,14 @@
+import Vue from 'vue'
+import App from './App.vue'
+
+import VueGmaps from 'vue-gmaps'
+
+Vue.use(VueGmaps, {
+  key: 'AIzaSyCpr35b_ZSoP8nbz0VnBjVz6ABb7iurRCU',
+  libraries: ['places']
+})
+
+new Vue({
+  el: '#app',
+  render: h => h(App)
+})
